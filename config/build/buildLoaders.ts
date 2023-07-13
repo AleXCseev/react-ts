0fd9277d3a2f65d832b0dc078253c6adb5fdd4f9
alekseev konstantin <alexcseev1990@gmail.com>
2023-07-13 23:04:33 +0300

    add webpack dev server and react

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,24 @@
 import webpack from "webpack"
 
 export function buildLoaders(): webpack.RuleSetRule[] {
-    return [
-        {
+
+    const cssLoaders = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+        ]
+    }
+
+    const typescriptLoader = {
             test: /\.tsx?$/,
             use: 'ts-loader',
             exclude: /node_modules/,
         }
+
+    return [
+        typescriptLoader,
+        cssLoaders
     ]
 }
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom"
+
+render(
+    <div>sfgsgsdg</div>,
+    document.getElementById("root")
+)
