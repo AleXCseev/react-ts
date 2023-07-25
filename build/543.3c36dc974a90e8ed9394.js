08774ba4ad61046e6895207715749d6eaad526c7
alekseev konstantin <alexcseev1990@gmail.com>
2023-07-25 22:39:25 +0300

    fix

--- build/543.3c36dc974a90e8ed9394.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkreact_ts=self.webpackChunkreact_ts||[]).push([[543],{543:(e,t,s)=>{s.r(t),s.d(t,{default:()=>r});var c=s(893);const r=function(){return(0,c.jsx)("h1",{children:"About Page"})}}}]);
