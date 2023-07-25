90f1fb321bad0d6bc167e20ffe8cf26f87d5c85a
alekseev konstantin <alexcseev1990@gmail.com>
2023-07-25 23:23:44 +0300

    fix

--- src/components/comp.tsx
+++
@@ -1,9 +0,0 @@
-import classes from  "./comp.module.scss";
-
-export default function Comp() {
-    return (
-        <div className={classes.comp}>
-            Hello world!!!!
-        </div>
-    )
-}
